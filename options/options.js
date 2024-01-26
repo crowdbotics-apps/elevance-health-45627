21bb24c88698511d9630ab53270eebf4ebc6128f
Crowdbotics <team@crowdbotics.com>
2024-01-26 22:45:49 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "elevance-health-45627",
+  url: "https://elevance-health-45627.botics.co",
+  api: "https://elevance-health-45627.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
